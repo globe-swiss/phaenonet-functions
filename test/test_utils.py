716946e3a3e8146e17745daab6201a265a38f00d
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2020-03-14 16:20:40 +0100

    fixed unit tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,7 +4,7 @@ import pytz
 from phenoback.gcloud.utils import *
 
 
-@pytest.skip
+@pytest.mark.skip
 @pytest.mark.parametrize('expected, context',
                          [('EDt26K5YIGoPe36z64vy_2020_BU_BFA', {'event_id': 'e0a433b0-188b-47d7-b4eb-95dd4a75f997-0', 'timestamp': '2020-03-08T14:52:13.469407Z', 'event_type': 'providers/cloud.firestore/eventTypes/document.write', 'resource': 'projects/phaenonet/databases/(default)/documents/observations/EDt26K5YIGoPe36z64vy_2020_BU_BFA'}),
                           ('5zhkvSSEUY5pRccOIAVf', {'event_id': 'c0453836-76a8-41d3-8b40-d1b42a412792-0', 'timestamp': '2020-03-08T14:52:13.646015Z', 'event_type': 'providers/cloud.firestore/eventTypes/document.create', 'resource': 'projects/phaenonet/databases/(default)/documents/activities/5zhkvSSEUY5pRccOIAVf'})
