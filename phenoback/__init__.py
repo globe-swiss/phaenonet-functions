b30d48b4da281da5893bc5081e2c2d24bc4aac16
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2020-04-16 09:57:22 +0200

    changed local credential location to ignore them more easily

--- phenoback/__init__.py
+++ phenoback/__init__.py
@@ -2,17 +2,15 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
-credential_file = os.path.join(os.path.dirname(__file__), '..', 'phaenonet-test-firebase-adminsdk-f8f66db4aeaa.json')
+credential_file = os.path.join(os.path.dirname(__file__), '..', 'credentials',
+                               'phaenonet-test-firebase-adminsdk-f8f66db4aeaa.json')
 
 if os.path.isfile(credential_file):
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_file
 
-    print(credential_file)
-
-    # Use a service account
     cred = credentials.Certificate(credential_file)
     firebase_admin.initialize_app(cred, {
         'storageBucket': 'phaenonet-test.appspot.com'
     })
 
-    print('INFO: app initialized for test instance')
+    print('INFO: app initialized with local credentials %s' % credential_file)
