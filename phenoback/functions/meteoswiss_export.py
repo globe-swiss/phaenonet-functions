faa74e3ad3fcc93fa8a74834f3f6cc3dd610723e
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2020-10-30 16:05:18 +0000

    dq issue handling on export

--- phenoback/functions/meteoswiss_export.py
+++ phenoback/functions/meteoswiss_export.py
@@ -2,7 +2,6 @@
 
 New PhaenoNet data mapped as good as possible to the previously existing export structure for meteoswiss.
 """
-# pragma: no cover
 import csv
 import io
 import logging
@@ -35,56 +34,59 @@ def process(year: int = None):
 
     results = []
     for o in observations:  # pylint: disable=invalid-name
-        i = individuals_map[o["individual"]]
-        results.append(
-            {
-                "OWNER": o["user"],
-                "MEAS_OBJ_ID": o["individual"],
-                "PLACENAME": i["name"],
-                "MEAS_YEAR": o["year"],
-                "MEAS_SPEC_ID": o["species"],
-                "MEAS_PPH_1": o["phenophase"],
-                "MEAS_ID": "",
-                "MEAS_DATE": o["date"].strftime("%d.%m.%Y"),
-                "MEAS_ALTGRP": "",
-                "MEAS_INCR": "",
-                "CREATED": o["created"].strftime("%d.%m.%Y %H:%M:%S"),
-                "MODIFIED": o["modified"].strftime("%d.%m.%Y %H:%M:%S")
-                if o["modified"]
-                else "",
-                "GEOPOS": "%s,%s" % (i["geopos"]["lat"], i["geopos"]["lng"]),
-                "ALTITUDE": i["altitude"],
-                "DESCRIPTION": i["description"],
-                "EXPOSITION": i["exposition"],
-                "GRADIENT": i["gradient"],
-                "SHADE": i["shade"],
-                "WATERING": i["watering"],
-                "LESS100": i["less100"],
-                "HABITAT": i["habitat"],
-                "FOREST": i["forest"],
-                "SPEC_ID": i["species"],
-                "ID": "",
-                "PARENT_ID": "",
-                "MEAS_PPH_2": "",
-                "NAME_DE": d.get_phenophase(o["species"], o["phenophase"])["de"],
-                "NAME_FR": "",
-                "NAME_EN": "",
-                "NAME_IT": "",
-                "FUNCTION": "MS_DATE",
-                "IN_SEQUENCE": "",
-                "MODIFIED_1": "",
-                "TENANT": "GLOBE_CH",
-                "FIRSTNAME": "",
-                "LASTNAME": "",
-                "ORGANISATION": "",
-                "MODIFIED_2": "",
-                "SPEC_SET_TENANT": d.get_species(o["species"])["de"],
-                "SPEC_SET_DE": "",
-                "SPEC_SET_FR": "",
-                "SPEC_SET_EN": "",
-                "SPEC_SET_IT": "",
-            }
-        )
+        try:
+            i = individuals_map[o["individual"]]
+            results.append(
+                {
+                    "OWNER": o["user"],
+                    "MEAS_OBJ_ID": o["individual"],
+                    "PLACENAME": i["name"],
+                    "MEAS_YEAR": o["year"],
+                    "MEAS_SPEC_ID": o["species"],
+                    "MEAS_PPH_1": o["phenophase"],
+                    "MEAS_ID": "",
+                    "MEAS_DATE": o["date"].strftime("%d.%m.%Y"),
+                    "MEAS_ALTGRP": "",
+                    "MEAS_INCR": "",
+                    "CREATED": o["created"].strftime("%d.%m.%Y %H:%M:%S"),
+                    "MODIFIED": o["modified"].strftime("%d.%m.%Y %H:%M:%S")
+                    if o["modified"]
+                    else "",
+                    "GEOPOS": "%s,%s" % (i["geopos"]["lat"], i["geopos"]["lng"]),
+                    "ALTITUDE": i["altitude"],
+                    "DESCRIPTION": i["description"],
+                    "EXPOSITION": i["exposition"],
+                    "GRADIENT": i["gradient"],
+                    "SHADE": i["shade"],
+                    "WATERING": i["watering"],
+                    "LESS100": i["less100"],
+                    "HABITAT": i["habitat"],
+                    "FOREST": i["forest"],
+                    "SPEC_ID": i["species"],
+                    "ID": "",
+                    "PARENT_ID": "",
+                    "MEAS_PPH_2": "",
+                    "NAME_DE": d.get_phenophase(o["species"], o["phenophase"])["de"],
+                    "NAME_FR": "",
+                    "NAME_EN": "",
+                    "NAME_IT": "",
+                    "FUNCTION": "MS_DATE",
+                    "IN_SEQUENCE": "",
+                    "MODIFIED_1": "",
+                    "TENANT": "GLOBE_CH",
+                    "FIRSTNAME": "",
+                    "LASTNAME": "",
+                    "ORGANISATION": "",
+                    "MODIFIED_2": "",
+                    "SPEC_SET_TENANT": d.get_species(o["species"])["de"],
+                    "SPEC_SET_DE": "",
+                    "SPEC_SET_FR": "",
+                    "SPEC_SET_EN": "",
+                    "SPEC_SET_IT": "",
+                }
+            )
+        except Exception:  # pylint: disable=broad-except
+            log.error("Error processing observation, skipping %s", o, exc_info=True)
 
     with io.StringIO() as csv_string:
         dict_writer = csv.DictWriter(csv_string, results[0].keys(), delimiter=";")
@@ -93,7 +95,7 @@ def process(year: int = None):
 
         storage.upload_string(
             None,
-            "exports/meteoswiss_%i.csv" % year,
+            "public/meteoswiss/export_%i.csv" % year,
             csv_string.getvalue(),
             content_type="text/csv",
         )
