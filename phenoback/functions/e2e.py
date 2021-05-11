5107518a64da7864972d15b5161cdc73b08944db
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-05-11 19:45:31 +0000

    e2e remove all invites

--- phenoback/functions/e2e.py
+++ phenoback/functions/e2e.py
@@ -9,3 +9,4 @@ log.setLevel(logging.INFO)
 def delete_user_individuals(user_id: str) -> None:
     log.info("Delete all individuals for %s", user_id)
     delete_batch("individuals", "user", "==", user_id)
+    delete_batch("invites", "user", "==", user_id)
