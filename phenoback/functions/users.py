0adc434686d14b63121f97bc227309fafe8d148c
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-06-03 09:46:41 +0000

    make user changes transactional

--- phenoback/functions/users.py
+++ phenoback/functions/users.py
@@ -1,17 +1,43 @@
-from phenoback.utils.firestore import delete_document, update_document, write_document
+from phenoback.utils.firestore import (
+    delete_document,
+    transaction,
+    transactional,
+    update_document,
+    write_document,
+)
 
 
-def process_new_user(user_id: str, nickname: str):
-    write_document("nicknames", nickname, {"user": user_id})
-    write_document("public_users", user_id, {"nickname": nickname})
+def process_new_user(user_id: str, nickname: str) -> None:
+    with transaction() as trx:
+        _process_new_user_trx(trx, user_id, nickname)
 
 
-def process_update_nickname(user_id: str, nickname_old: str, nickname_new: str):
-    write_document("nicknames", nickname_new, {"user": user_id})
-    update_document("public_users", user_id, {"nickname": nickname_new})
-    delete_document("nicknames", nickname_old)
+@transactional
+def _process_new_user_trx(trx, user_id, nickname):
+    write_document("nicknames", nickname, {"user": user_id}, transaction=trx)
+    write_document("public_users", user_id, {"nickname": nickname}, transaction=trx)
 
 
-def process_delete_user(user_id: str, nickname: str):
-    delete_document("nicknames", nickname)
-    delete_document("public_users", user_id)
+def process_update_nickname(user_id: str, nickname_old: str, nickname_new: str) -> None:
+    with transaction() as trx:
+        _process_update_nickname_trx(trx, user_id, nickname_old, nickname_new)
+
+
+@transactional
+def _process_update_nickname_trx(trx, user_id, nickname_old, nickname_new):
+    write_document("nicknames", nickname_new, {"user": user_id}, transaction=trx)
+    update_document(
+        "public_users", user_id, {"nickname": nickname_new}, transaction=trx
+    )
+    delete_document("nicknames", nickname_old, transaction=trx)
+
+
+def process_delete_user(user_id: str, nickname: str) -> None:
+    with transaction() as trx:
+        _process_delete_user_trx(trx, user_id, nickname)
+
+
+@transactional
+def _process_delete_user_trx(trx, user_id, nickname):
+    delete_document("nicknames", nickname, transaction=trx)
+    delete_document("public_users", user_id, transaction=trx)
