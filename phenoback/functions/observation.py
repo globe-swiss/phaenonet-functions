6d6bc08f30d5304ed7050ab6e3211bb53977b025
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2022-03-25 20:04:33 +0000

    fix: update last phenophase on delete observation

--- phenoback/functions/observation.py
+++ phenoback/functions/observation.py
@@ -1,38 +1,99 @@
 import logging
 from datetime import datetime, timezone
+from typing import Optional
 
-from phenoback.utils.data import get_individual
-from phenoback.utils.firestore import update_document
+import phenoback.utils.data as d
+import phenoback.utils.firestore as f
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def update_last_observation(
+def _update_individual(
+    individual_id: str,
+    individual_type: str,
+    phase: str,
+    old_observation_date: datetime,
+    new_observation_date: datetime,
+):
+    data = {"last_observation_date": new_observation_date}
+    if individual_type == "individual":
+        data["last_phenophase"] = phase
+
+    d.update_individual(individual_id, data)
+    log.info(
+        "updated last observation for %s (%s -> %s)",
+        individual_id,
+        old_observation_date,
+        new_observation_date,
+    )
+
+
+def updated_observation(
     individual_id: str, phase: str, observation_date: datetime
 ) -> bool:
-    individual = get_individual(individual_id)
+    individual = d.get_individual(individual_id)
     old_observation_date = individual.get(
         "last_observation_date", datetime.min.replace(tzinfo=timezone.utc)
     )
-    if observation_date > old_observation_date:
-        data = {"last_observation_date": observation_date}
-        if individual.get("type") == "individual":
-            data["last_phenophase"] = phase
-
-        update_document("individuals", individual_id, data)
-        log.info(
-            "updated last observation for %s (%s -> %s)",
+    if observation_date >= old_observation_date:
+        _update_individual(
             individual_id,
+            individual.get("type"),
+            phase,
             old_observation_date,
             observation_date,
         )
         return True
     else:
         log.info(
-            "no update for last observation for %s (%s > %s)",
+            "update: no update for last observation for %s (%s > %s)",
             individual_id,
             old_observation_date,
             observation_date,
         )
         return False
+
+
+def removed_observation(individual_id: str, observation_date: datetime) -> bool:
+    individual = d.get_individual(individual_id)
+    old_observation_date = individual.get(
+        "last_observation_date", datetime.min.replace(tzinfo=timezone.utc)
+    )
+    if observation_date == old_observation_date:
+        last_observation = _get_last_observation(individual_id)
+        individual_type = individual.get("type")
+        if last_observation:
+            new_phenophase = last_observation.get("phenophase")
+            new_observation_date = last_observation.get("date")
+        else:
+            new_phenophase = f.DELETE_FIELD
+            new_observation_date = f.DELETE_FIELD
+        _update_individual(
+            individual_id,
+            individual_type,
+            new_phenophase,
+            old_observation_date,
+            new_observation_date,
+        )
+        return True
+    else:
+        log.debug(
+            "remove: no update for last observation for %s (%s != %s)",
+            individual_id,
+            old_observation_date,
+            observation_date,
+        )
+        return False
+
+
+def _get_last_observation(individual_id: str) -> Optional[dict]:
+    last_obs_query = (
+        d.query_observation("individual_id", "==", individual_id)
+        .order_by("date", "DESCENDING")
+        .limit(1)
+    )
+    for obs in last_obs_query.stream():
+        print(obs.to_dict())
+        return obs.to_dict()
+    return None
--- test/test_observations.py
+++ test/test_observations.py
@@ -1,63 +1,230 @@
-from datetime import datetime
+# pylint: disable=protected-access
+
+from datetime import datetime, timezone
 
 import pytest
 
+import phenoback.utils.data as d
 from phenoback.functions import observation
 
 
+@pytest.fixture()
+def individual_id():
+    d.write_individual(
+        "individual_id",
+        {
+            "type": "individual",
+            "last_phenophase": "old_pp",
+            "last_observation_date": datetime(2020, 1, 1, tzinfo=timezone.utc),
+        },
+    )
+    return "individual_id"
+
+
+@pytest.fixture()
+def station_id():
+    d.write_individual(
+        "station_id",
+        {
+            "type": "station",
+            "last_observation_date": datetime(2020, 1, 1, tzinfo=timezone.utc),
+        },
+    )
+    return "station_id"
+
+
+@pytest.fixture()
+def last_observation():
+    d.write_observation(
+        "obs_1",
+        {
+            "individual_id": "individual_id",
+            "date": datetime(2020, 1, 1, tzinfo=timezone.utc),
+            "phenophase": "A",
+        },
+    )
+    d.write_observation(
+        "obs_2",
+        {
+            "individual_id": "individual_id",
+            "date": datetime(2020, 1, 2, tzinfo=timezone.utc),
+            "phenophase": "B",
+        },
+    )
+    d.write_observation(
+        "obs_3",
+        {
+            "individual_id": "individual_id",
+            "date": datetime(2020, 1, 3, tzinfo=timezone.utc),
+            "phenophase": "C",
+        },
+    )
+    return (
+        "individual_id",
+        {
+            "individual_id": "individual_id",
+            "date": datetime(2020, 1, 3, tzinfo=timezone.utc),
+            "phenophase": "C",
+        },
+    )
+
+
 @pytest.mark.parametrize(
-    "new_date, old_date, expected",
+    "new_date, old_date, update_expected",
     [
-        (datetime(2020, 1, 3), datetime(2020, 1, 3), False),
-        (datetime(2020, 1, 2), datetime(2020, 1, 3), False),
-        (datetime(2020, 1, 4), datetime(2020, 1, 3), True),
+        (
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            True,
+        ),
+        (
+            datetime(2020, 1, 2, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            False,
+        ),
+        (
+            datetime(2020, 1, 4, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            True,
+        ),
     ],
 )
-def test_update_last_observation_status(mocker, new_date, old_date, expected):
-    mocker.patch(
-        "phenoback.functions.observation.get_individual",
-        return_value={"last_observation_date": old_date},
-    )
-    mocker.patch("phenoback.functions.observation.update_document")
+def test_update_last_observation__individual(
+    individual_id, new_date, old_date, update_expected
+):
+    d.update_individual(individual_id, {"last_observation_date": old_date})
 
-    assert expected == observation.update_last_observation(
-        "ignored", "ignored", new_date
+    assert update_expected == observation.updated_observation(
+        individual_id, "new_pp", new_date
+    )
+    updated_individual = d.get_individual(individual_id)
+    assert (
+        updated_individual.get("last_observation_date") == new_date
+        if update_expected
+        else old_date
+    )
+    assert (
+        updated_individual.get("last_phenophase") == "new_pp"
+        if update_expected
+        else "old_pp"
     )
 
 
 @pytest.mark.parametrize(
-    "observation_type, expected", [("station", False), ("individual", True)]
+    "new_date, old_date, update_expected",
+    [
+        (
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            True,
+        ),
+        (
+            datetime(2020, 1, 2, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            False,
+        ),
+        (
+            datetime(2020, 1, 4, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            True,
+        ),
+    ],
 )
-def test_update_last_observation_phenophase(mocker, observation_type, expected):
+def test_update_last_observation__station(
+    station_id, new_date, old_date, update_expected
+):
+    d.update_individual(station_id, {"last_observation_date": old_date})
+
+    assert update_expected == observation.updated_observation(
+        station_id, "new_pp", new_date
+    )
+    updated_station = d.get_individual(station_id)
+    assert (
+        updated_station.get("last_observation_date") == new_date
+        if update_expected
+        else old_date
+    )
+    assert updated_station.get("last_phenophase") is None
+
+
+@pytest.mark.parametrize(
+    "current_last_observation_date, removed_last_observation_date, update_expected",
+    [
+        (
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            True,
+        ),
+        (
+            datetime(2020, 1, 2, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            False,
+        ),
+        (
+            datetime(2020, 1, 4, tzinfo=timezone.utc),
+            datetime(2020, 1, 3, tzinfo=timezone.utc),
+            False,
+        ),
+    ],
+)
+def test_removed_observation(
+    mocker,
+    individual_id,
+    current_last_observation_date,
+    removed_last_observation_date,
+    update_expected,
+):
+    new_last_observation = {
+        "date": datetime(2022, 1, 1, tzinfo=timezone.utc),
+        "phenophase": "new_pp",
+    }
     mocker.patch(
-        "phenoback.functions.observation.get_individual",
-        return_value={
-            "last_observation_date": datetime(2020, 1, 1),
-            "type": observation_type,
-        },
+        "phenoback.functions.observation._get_last_observation",
+        return_value=new_last_observation,
     )
-    update_mock = mocker.patch("phenoback.functions.observation.update_document")
-
-    assert observation.update_last_observation(
-        "ignored", "ignored", datetime(2020, 1, 2)
+    d.update_individual(
+        individual_id, {"last_observation_date": current_last_observation_date}
     )
-    update_mock.assert_called_once()
-    assert ("last_phenophase" in update_mock.call_args[0][2].keys()) == expected
+
+    assert update_expected == observation.removed_observation(
+        individual_id, removed_last_observation_date
+    )
+    updated_individual = d.get_individual(individual_id)
+    assert (
+        updated_individual.get("last_observation_date")
+        == new_last_observation.get("date")
+        if update_expected
+        else current_last_observation_date
+    )
+    assert (
+        updated_individual.get("last_phenophase") == "new_pp"
+        if update_expected
+        else "old_pp"
+    )
 
 
-def test_update_last_observation_update_values(mocker):
+def test_removed_observation__no_observations(mocker, individual_id):
+    new_last_observation = None
+    current_last_observation_date = datetime(2020, 1, 3, tzinfo=timezone.utc)
+    removed_last_observation_date = datetime(2020, 1, 3, tzinfo=timezone.utc)
     mocker.patch(
-        "phenoback.functions.observation.get_individual",
-        return_value={
-            "last_observation_date": datetime(2020, 1, 3),
-            "type": "individual",
-        },
+        "phenoback.functions.observation._get_last_observation",
+        return_value=new_last_observation,
     )
-    update_mock = mocker.patch("phenoback.functions.observation.update_document")
+    d.update_individual(
+        individual_id, {"last_observation_date": current_last_observation_date}
+    )
 
-    assert observation.update_last_observation(
-        "ignored", "a_phenophase", datetime(2020, 1, 10)
-    )
-    update_mock.assert_called_once()
-    assert update_mock.call_args[0][2]["last_observation_date"] == datetime(2020, 1, 10)
-    assert update_mock.call_args[0][2]["last_phenophase"] == "a_phenophase"
+    assert observation.removed_observation(individual_id, removed_last_observation_date)
+    updated_individual = d.get_individual(individual_id)
+    assert updated_individual.get("last_observation_date") is None
+    assert updated_individual.get("last_phenophase") is None
+
+
+def test_get_last_observation(last_observation):
+    print(last_observation[0])
+    assert observation._get_last_observation(last_observation[0]) == last_observation[1]
+
+
+def test_get_last_observation__no_observations():
+    assert observation._get_last_observation("no_observations") is None
