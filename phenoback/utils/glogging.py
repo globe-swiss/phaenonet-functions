f3ea5c139a6126904be1ad9a9b2e280b71ac9e5f
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2022-07-13 08:58:40 +0000

    fix logging for python 3.9
    logging with severities now works as documented and
    fixes broken exception logging

--- phenoback/utils/glogging.py
+++ phenoback/utils/glogging.py
@@ -1,24 +1,6 @@
-import logging
-
 import google.cloud.logging
-from google.cloud.logging_v2.handlers import CloudLoggingHandler
-from google.cloud.logging_v2.resource import Resource
-
-from phenoback.utils import gcloud
 
 
-def init(log_id="Unknown"):  # pragma: no cover
-    resource = Resource(
-        type="cloud_function",
-        labels={
-            "function_name": gcloud.get_function_name(),
-            "project_id": gcloud.get_project(),
-        },
-    )
-
+def init():  # pragma: no cover
     client = google.cloud.logging.Client()
-    logging.getLogger().handlers = []
-    handler = CloudLoggingHandler(
-        client, resource=resource, labels={"log_id": str(log_id)}
-    )
-    logging.getLogger().addHandler(handler)
+    client.setup_logging()
