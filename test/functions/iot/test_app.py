e9d2f84c25fffe6ae93573aaba1dcb45ce641341
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2023-02-03 10:15:06 +0000

    adapt sensor registration

--- test/functions/iot/test_app.py
+++ test/functions/iot/test_app.py
@@ -3,13 +3,12 @@ from test.functions.iot.sample_data import DraginoData as dd
 from zoneinfo import ZoneInfo
 
 import pytest
-from flask import Request
 from freezegun import freeze_time
-from werkzeug.test import EnvironBuilder
 
 from phenoback.functions.iot import app
 from phenoback.utils import data as d
 from phenoback.utils import firestore as f
+from phenoback.utils import gcloud as g
 
 YEAR = 2000
 INDIVIDUAL = "individual"
@@ -20,25 +19,48 @@ def set_phenoyear():
     f.write_document("definitions", "config_dynamic", {"phenoyear": YEAR})
 
 
-@pytest.fixture()
-def individual_id():
-    individual_id = "id"
-    d.write_individual(
-        individual_id, {"deveui": dd.DEVEUI, "year": YEAR, "individual": INDIVIDUAL}
-    )
-    return individual_id
+# @pytest.fixture()
+# def individual_id():
+#     individual_id = "id"
+#     d.write_individual(
+#         individual_id, {"deveui": dd.DEVEUI, "year": YEAR, "individual": INDIVIDUAL}
+#     )
+#     return individual_id
 
 
-@pytest.fixture()
-def individual_id_no_deveui():
-    individual_id = "id_no_deveui"
-    d.write_individual(individual_id, {"year": YEAR, "individual": INDIVIDUAL})
-    return individual_id
+# @pytest.fixture()
+# def individual_id_no_deveui():
+#     individual_id = "id_no_deveui"
+#     d.write_individual(individual_id, {"year": YEAR, "individual": INDIVIDUAL})
+#     return individual_id
+
+
+def add_individual(individual_id, individual, year, deveui=None):
+    if deveui:
+        data = {
+            "deveui": deveui,
+            "year": year,
+            "individual": individual,
+            "sensor": {"foo": "bar"},
+        }
+    else:
+        data = {"year": year, "individual": individual}
+    d.write_individual(individual_id, data)
 
 
 @pytest.fixture
 def increase_uplink_frequency_mock(mocker):
     return mocker.patch("phenoback.functions.iot.app.increase_uplink_frequency")
+
+
+@pytest.fixture
+def sensor_set_mock(mocker):
+    return mocker.patch("phenoback.functions.iot.app.sensor_set")
+
+
+@pytest.fixture
+def remove_sensor_mock(mocker):
+    return mocker.patch("phenoback.functions.iot.app.remove_sensor")
 
 
 @pytest.fixture(autouse=True)
@@ -58,57 +80,83 @@ def test_main(mocker, pubsub_event, context):
     process_mock.assert_called_with({"foo": "bar"})
 
 
-def test_main_set_sensor__ok(mocker):
-    set_sensor_mock = mocker.patch(
-        "phenoback.functions.iot.app.set_sensor", return_value=True
-    )
-    payload = {"individual": "foo", "deveui": "bar", "year": 2000}
-    request = Request(
-        EnvironBuilder(
-            method="POST",
-            json=payload,
-        ).get_environ()
-    )
-
-    result = app.main_set_sensor(request)
-
-    assert result.status_code == 200
-    set_sensor_mock.assert_called_with("foo", 2000, "bar")
-
-
 @pytest.mark.parametrize(
-    "payload, status",
+    "action, data",
     [
-        ({"individual": "foo"}, 400),
-        ({"deveui": "bar"}, 400),
         (
+            "add",
             {
-                "individual": "foo",
-                "deveui": "bar",
+                "updateMask": {"fieldPaths": ["deveui"]},
+                "oldValue": {},
+                "value": {
+                    "fields": {
+                        "deveui": {"StringValue": "deveui_new"},
+                        "individual": {"StringValue": "individual"},
+                    }
+                },
             },
-            400,
         ),
         (
-            {"individual": "foo", "deveui": "bar", "year": 2000},
-            404,
+            "update",
+            {
+                "updateMask": {"fieldPaths": ["deveui"]},
+                "oldValue": {
+                    "fields": {
+                        "deveui": {"StringValue": "deveui_old"},
+                        "individual": {"StringValue": "individual"},
+                    }
+                },
+                "value": {
+                    "fields": {
+                        "deveui": {"StringValue": "deveui_new"},
+                        "individual": {"StringValue": "individual"},
+                    }
+                },
+            },
         ),
     ],
 )
-def test_main_set_sensor__error(mocker, payload, status):
-    mocker.patch("phenoback.functions.iot.app.set_sensor", return_value=False)
-    request = Request(
-        EnvironBuilder(
-            method="POST",
-            json=payload,
-        ).get_environ()
+def test_main_individual_updated(
+    sensor_set_mock, remove_sensor_mock, data, context, action
+):
+    app.main_individual_updated(data, context)
+
+    remove_sensor_mock.assert_not_called()
+    sensor_set_mock.assert_called_with(
+        g.get_document_id(context), "individual", "deveui_new"
     )
 
-    result = app.main_set_sensor(request)
 
-    assert result.status_code == status
+@pytest.mark.parametrize(
+    "action, data",
+    [
+        (
+            "delete",
+            {
+                "updateMask": {"fieldPaths": ["deveui"]},
+                "oldValue": {
+                    "fields": {
+                        "deveui": {"StringValue": "deveui_old"},
+                        "individual": {"StringValue": "individual"},
+                    }
+                },
+                "value": {},
+            },
+        ),
+    ],
+)
+def test_main_individual_updated__delete(
+    sensor_set_mock, remove_sensor_mock, data, context, action
+):
+    app.main_individual_updated(data, context)
+
+    sensor_set_mock.assert_not_called()
+    remove_sensor_mock.assert_called_with(g.get_document_id(context))
 
 
-def test_process_dragino__e2e(mocker, individual_id):
+def test_process_dragino__e2e(mocker):
+    individual_id = "id1"
+    add_individual(individual_id, "individual", YEAR, dd.DEVEUI)
     update_spy = mocker.spy(app, "update")
     app.process_dragino(dd.SAMPLE_DATA)
 
@@ -127,12 +175,15 @@ def test_process_dragino__individual_not_found(mocker, capwarnings):
     assert len(capwarnings.records) == 1, capwarnings.records
 
 
-def test_get_individual_id(individual_id):
-    assert app.get_individual_id(YEAR, dd.DEVEUI) == individual_id
+def test_get_individual_id():
+    add_individual("id1", "ind1", 2000, deveui="deveui1")
+    add_individual("id2", "ind2", 2000, deveui="deveui2")
+    add_individual("id3", "ind2", 2001, deveui="deveui2")
+    assert app.get_individual_id(2000, "deveui2") == "id2"
 
 
 def test_get_individual_id__not_found():
-    assert app.get_individual_id(YEAR, dd.DEVEUI) is None
+    assert app.get_individual_id(2000, "unknown_deveui") is None
 
 
 def test_update(mocker):
@@ -175,7 +226,9 @@ def test_update_history__two():
     assert result.get("n") == 2, result
 
 
-def test_update_individual(individual_id):
+def test_update_individual():
+    individual_id = "ind_id"
+    add_individual(individual_id, "individual", 2000)
     app.update_individual(individual_id, 1.0, 1.1, 2.0, 2.1)
 
     result = d.get_individual(individual_id)["sensor"]
@@ -186,72 +239,66 @@ def test_update_individual(individual_id):
     assert result["ts"]
 
 
-def test_set_sensor__new(increase_uplink_frequency_mock, individual_id_no_deveui):
-    result = app.set_sensor(INDIVIDUAL, YEAR, dd.DEVEUI)
+def test_set_sensor__new(remove_sensor_mock, increase_uplink_frequency_mock):
+    add_individual("id1", "ind1", 2000, deveui="deveui1")
+    add_individual("id2", "ind2", 2000)
 
-    individual = d.get_individual(individual_id_no_deveui)
+    app.sensor_set("id1", "ind1", "deveui1")
 
-    assert result
-    assert individual.get("deveui") == dd.DEVEUI
-    increase_uplink_frequency_mock.assert_called_with(dd.DEVEUI)
+    remove_sensor_mock.assert_not_called()
+    increase_uplink_frequency_mock.assert_called_with("deveui1")
 
 
-def test_set_sensor__switch(
-    increase_uplink_frequency_mock, individual_id, individual_id_no_deveui
+def test_set_sensor__switch_individual(
+    remove_sensor_mock,
+    increase_uplink_frequency_mock,
 ):
-    d.update_individual(individual_id, {"sensor": "some data"})
-    init_old = d.get_individual(individual_id)
-    init_new = d.get_individual(individual_id_no_deveui)
-    assert init_old.get("deveui") == dd.DEVEUI
-    assert init_old.get("sensor")
-    assert not init_new.get("deveui")
+    add_individual("id1", "ind1", 2000, deveui="deveui1")
+    add_individual("id2", "ind2", 2000, deveui="deveui1")
 
-    result = app.set_sensor(INDIVIDUAL, YEAR, dd.DEVEUI)
+    app.sensor_set("id2", "ind2", "deveui1")
 
-    individual_old = d.get_individual(individual_id)
-    individual_new = d.get_individual(individual_id_no_deveui)
-
-    assert result
-    assert individual_new.get("deveui") == dd.DEVEUI
-    assert not individual_old.get("deveui")
-    assert not individual_old.get("sensor")
-    increase_uplink_frequency_mock.assert_called_with(dd.DEVEUI)
+    remove_sensor_mock.assert_called_with("id1")
+    increase_uplink_frequency_mock.assert_called_with("deveui1")
 
 
-def test_set_sensor__not_found(increase_uplink_frequency_mock):
-    result = app.set_sensor("foo", YEAR, dd.DEVEUI)
+def test_set_sensor__switch_year(
+    remove_sensor_mock,
+    increase_uplink_frequency_mock,
+):
+    add_individual("id1", "ind1", 1999, deveui="deveui1")
+    add_individual("id2", "ind1", 2000, deveui="deveui1")
 
-    assert not result
-    increase_uplink_frequency_mock.assert_not_called()
+    app.sensor_set("id2", "ind2", "deveui1")
 
-
-def test_remove_sensor(individual_id):
-    d.update_individual(individual_id, {"sensor": "some data"})
-    init = d.get_individual(individual_id)
-    assert init.get("deveui") == dd.DEVEUI
-    assert init.get("sensor")
-
-    result = app.remove_sensor(dd.DEVEUI, YEAR)
-
-    individual = d.get_individual(individual_id)
-
-    assert result
-    assert not individual.get("deveui")
-    assert not individual.get("sensor")
+    remove_sensor_mock.assert_called_with("id1")
+    increase_uplink_frequency_mock.assert_called_with("deveui1")
 
 
-def test_remove_sensor__not_found():
-    result = app.remove_sensor(dd.DEVEUI, YEAR)
-    assert not result
+def test_remove_sensor():
+    add_individual("id1", "ind1", 2000, deveui="deveui1")
+    add_individual("id2", "ind2", 2000, deveui="deveui2")
+
+    app.remove_sensor("id2")
+
+    assert d.get_individual("id1")["deveui"] == "deveui1"
+    assert d.get_individual("id1")["sensor"]  # not empty
+
+    assert not d.get_individual("id2").get("deveui")  # removed
+    assert not d.get_individual("id2")["sensor"]  # empty
 
 
-def test_clear_sensors(individual_id):
-    d.update_individual(individual_id, {"sensor": "some data"})
+def test_clear_sensors():
+    individual_id = "id1"
+    add_individual(individual_id, "individual", 2000, "some_deveui")
+    add_individual("id2", "individual", 2000, "some_deveui")
+    add_individual("id3", "individual", 1999, "some_deveui")
+    assert d.get_individual(individual_id)["sensor"]  # has data
 
     result = app.clear_sensors(YEAR)
 
-    assert result == 1
-    assert not d.get_individual(individual_id).get("sensor")
+    assert result == 2
+    assert not d.get_individual(individual_id)["sensor"]  # present but empty
 
 
 @freeze_time(datetime.datetime(2020, 1, 1, tzinfo=ZoneInfo("Europe/Zurich")))
