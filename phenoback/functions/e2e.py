ab3fa5ea41673e68604a70148a58f67a3c1525b8
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2023-02-14 14:39:05 +0100

    clean e2e-ranger data (#162)

--- phenoback/functions/e2e.py
+++ phenoback/functions/e2e.py
@@ -13,11 +13,13 @@ def main(request: Request):  # pylint: disable=unused-argument
     """
     Clear all individuals for the e2e test user. This is used for assuring the firestore state before running e2e tests.
     """
-    delete_user_individuals("q7lgBm5nm7PUkof20UdZ9D4d0CV2")
+    delete_user_individuals(
+        ["q7lgBm5nm7PUkof20UdZ9D4d0CV2", "JIcn8kFpI4fYYcbdi9QzPlrHomn1"]
+    )
     return Response("ok", HTTPStatus.OK)
 
 
-def delete_user_individuals(user_id: str) -> None:
-    log.info("Delete all individuals for %s", user_id)
-    delete_batch("individuals", "user", "==", user_id)
-    delete_batch("invites", "user", "==", user_id)
+def delete_user_individuals(user_ids: str) -> None:
+    log.info("Delete all data for %s", user_ids)
+    delete_batch("individuals", "user", "in", user_ids)
+    delete_batch("invites", "user", "in", user_ids)
--- test/functions/test_e2e.py
+++ test/functions/test_e2e.py
@@ -9,17 +9,19 @@ def test_main(mocker):
     e2e_mock = mocker.patch("phenoback.functions.e2e.delete_user_individuals")
     assert isinstance(e2e.main("ignored"), Response)
     e2e_mock.assert_called_once()
-    e2e_mock.assert_called_with("q7lgBm5nm7PUkof20UdZ9D4d0CV2")
+    e2e_mock.assert_called_with(
+        ["q7lgBm5nm7PUkof20UdZ9D4d0CV2", "JIcn8kFpI4fYYcbdi9QzPlrHomn1"]
+    )
 
 
 def test_delete_all_individuals():
     d.write_individual("u1_i1", {"user": "u1"})
     d.write_individual("u1_i2", {"user": "u1"})
     d.write_individual("u2_i1", {"user": "u2"})
-    d.write_individual("u2_i1", {"user": "u2"})
+    d.write_individual("u2_i2", {"user": "u2"})
+    d.write_individual("u3_i1", {"user": "u3"})
+    d.write_individual("u3_i2", {"user": "u3"})
 
-    e2e.delete_user_individuals("u1")
-    for individual in d.query_individuals("user", "==", "u1").stream():
-        assert False, individual
+    e2e.delete_user_individuals(["u1", "u3"])
     for individual in f.get_collection("individuals").stream():
         assert individual.to_dict()["user"] == "u2"
