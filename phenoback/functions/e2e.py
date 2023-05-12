d11429796f9e8e858b4ec2fa04000d5ff3429d74
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2023-05-12 16:54:20 +0200

    clean e2e followings (#163)

--- phenoback/functions/e2e.py
+++ phenoback/functions/e2e.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 
 from flask import Request, Response
 
-from phenoback.utils.firestore import delete_batch
+import phenoback.utils.firestore as f
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -13,13 +13,21 @@ def main(request: Request):  # pylint: disable=unused-argument
     """
     Clear all individuals for the e2e test user. This is used for assuring the firestore state before running e2e tests.
     """
-    delete_user_individuals(
-        ["q7lgBm5nm7PUkof20UdZ9D4d0CV2", "JIcn8kFpI4fYYcbdi9QzPlrHomn1"]
-    )
+    delete_user_data(["q7lgBm5nm7PUkof20UdZ9D4d0CV2", "JIcn8kFpI4fYYcbdi9QzPlrHomn1"])
     return Response("ok", HTTPStatus.OK)
 
 
-def delete_user_individuals(user_ids: str) -> None:
+def delete_user_data(user_ids: str) -> None:
     log.info("Delete all data for %s", user_ids)
-    delete_batch("individuals", "user", "in", user_ids)
-    delete_batch("invites", "user", "in", user_ids)
+    f.delete_batch("individuals", "user", "in", user_ids)
+    f.delete_batch("invites", "user", "in", user_ids)
+    for user_id in user_ids:
+        f.write_document(
+            "users",
+            user_id,
+            {
+                "following_individuals": f.DELETE_FIELD,
+                "following_users": f.DELETE_FIELD,
+            },
+            merge=True,
+        )
--- test/functions/test_e2e.py
+++ test/functions/test_e2e.py
@@ -6,7 +6,7 @@ from phenoback.utils import firestore as f
 
 
 def test_main(mocker):
-    e2e_mock = mocker.patch("phenoback.functions.e2e.delete_user_individuals")
+    e2e_mock = mocker.patch("phenoback.functions.e2e.delete_user_data")
     assert isinstance(e2e.main("ignored"), Response)
     e2e_mock.assert_called_once()
     e2e_mock.assert_called_with(
@@ -14,7 +14,7 @@ def test_main(mocker):
     )
 
 
-def test_delete_all_individuals():
+def test_delete_individuals():
     d.write_individual("u1_i1", {"user": "u1"})
     d.write_individual("u1_i2", {"user": "u1"})
     d.write_individual("u2_i1", {"user": "u2"})
@@ -22,6 +22,32 @@ def test_delete_all_individuals():
     d.write_individual("u3_i1", {"user": "u3"})
     d.write_individual("u3_i2", {"user": "u3"})
 
-    e2e.delete_user_individuals(["u1", "u3"])
+    e2e.delete_user_data(["u1", "u3"])
     for individual in f.get_collection("individuals").stream():
         assert individual.to_dict()["user"] == "u2"
+
+
+def test_remove_following():
+    d.write_document(
+        "users",
+        "u1",
+        {"foo": "bar", "following_individuals": "foo", "following_users": "bar"},
+    )
+    d.write_document(
+        "users",
+        "u2",
+        {"foo": "bar", "following_individuals": "foo", "following_users": "bar"},
+    )
+    d.write_document("users", "u3", {"foo": "bar"})
+
+    e2e.delete_user_data(["u1", "u3"])
+    results = list(f.get_collection("users").stream())
+    assert len(results) == 3
+    for user in results:
+        assert user.to_dict().get("foo"), user.to_dict()
+        if user.id == "u2":
+            assert user.to_dict().get("following_individuals"), user.to_dict()
+            assert user.to_dict().get("following_users"), user.to_dict()
+        else:
+            assert not user.to_dict().get("following_individuals"), user.to_dict()
+            assert not user.to_dict().get("following_users"), user.to_dict()
